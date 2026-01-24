d38ee6bbd374d475f85e71d13c50aa98eb09d214
Anuska Biswas <anuskabiswas2027@gmail.com>
2026-01-24 22:43:17 +0530

    commited

--- Stack&Queue/greaterfreequency.cpp
+++ Stack&Queue/greaterfreequency.cpp
@@ -1,2 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
+class Solution {
+  public:
+    vector<int> nextFreqGreater(vector<int>& arr) {
+        
+        // code here
+        unordered_map<int,int>mp;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]]++;
+        }
+        stack<int>st;
+        vector<int>ans(n);
+        for(int i=n-1;i>=0;i--){
+        while(!st.empty() && mp[arr[i]]>=mp[st.top()])
+        {
+            st.pop();
+        }
+        if(st.empty())
+        {
+            ans[i]=-1;
+        }
+        else{
+           ans[i]=st.top();
+        }
+        
+        st.push(arr[i]);
+        }
+        return ans;
+    }
+};
